d8add47414ee8578b4bc6a48f12e0c1ce4914360
Nicholas Karlson <nicholaskarlson@gmail.com>
2025-11-10 17:02:51 -0800

    feat(ch13): add tests, fix CI data size, and run tests in workflow (#51)

--- tests/test_ch13_fitness.py
+++ tests/test_ch13_fitness.py
@@ -4,7 +4,8 @@ from scripts import sim_fitness_2x2 as sim
 SEED = 2025
 
 def test_fitness_mixed_model_signals():
-    subjects, long_df = sim.simulate(n_per_group=5, seed=SEED)
+    # Use n_per_group=40 to provide enough data for the model
+    subjects, long_df = sim.simulate(n_per_group=40, seed=SEED)
 
     # Mixed model with random intercept for id, categorical time/group + covariates
     md = smf.mixedlm(
@@ -18,6 +19,7 @@ def test_fitness_mixed_model_signals():
     assert res.params["C(time)[T.post]"] > 5
     assert res.pvalues["C(time)[T.post]"] < 1e-6
 
-    # interaction should be positive (ProgramB a bit more improvement); allow modest p
+    # interaction should be positive (ProgramB a bit more improvement)
     assert res.params["C(time)[T.post]:C(group)[T.ProgramB]"] > 0
-    assert res.pvalues["C(time)[T.post]:C(group)[T.ProgramB]"] < 0.2
+    # With N=80, we can have a stricter p-value check
+    assert res.pvalues["C(time)[T.post]:C(group)[T.ProgramB]"] < 0.05
